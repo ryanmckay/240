6803654d0c7c9aa0a2e8fd4054a5fa71dc161196
Andy Yee <andidious@gmail.com>
2013-08-02 12:44:41 -0400

    Still working on friend face. Loading in connection vectors for each users

--- FriendFace.cpp
+++ FriendFace.cpp
@@ -20,8 +20,10 @@ using namespace std;
 
 	/* File parsing and set up user vector */
 	void FriendFace::setup(vector<User>& userVec, string file_name) {
-		int totalUsers;
-		string temp;
+		/* count is used first as totalUsers, then connectionCount for
+		each user */
+		int userCount, connectionCount;
+		string temp, temp2;
 		
 		cout << "Parsing file: " << file_name << "...";
 		ifstream inFile(file_name.c_str());	
@@ -36,18 +38,43 @@ using namespace std;
 		/* Get first line, check # of users */
 		getline(inFile,temp);
 		stringstream ss(temp);
-		ss >> totalUsers;
-		cout << "total: " << totalUsers << endl;
+		ss >> userCount;
+		cout << "total: " << userCount << endl;
 
 		/* Create a user object for each user.
 		We will fill in their connections after. */
-		for( int i=0 ; i < totalUsers ; i++ ) {
+		for( int i=0 ; i < userCount ; i++ ) {
 			getline(inFile,temp);
 			User u = User(temp);
 			userVec.push_back(u);	
 		}
+		
+		/* Start filling each user's connections */
+		while( userCount > 0 ) {
+			getline(inFile,temp,',');
+			cout << temp << endl;
+			getline(inFile,temp2);
+			cout << temp2 << endl;
+			/* clearing ss */
+			ss.str("");
+			ss.clear();
+			
+			ss.str(temp);
+			userCount--;
+		}
 	}
 
+/* HELPER METHODS */
+
+	/* Trim right and left white-space for string */
+	void FriendFace::trim(string& s) {
+		size_t i = s.find_first_not_of(" \t");
+		s.erase(0, i);
+		i = s.find_last_not_of(" \t");
+		if (string::npos != i)
+		s.erase(i+1);
+	}
+	
 	void FriendFace::listUsers() {
 		for(vector<User>::iterator it = userVec.begin(); it != userVec.end(); it++){
 			cout << userVec.at(it-userVec.begin()).name << endl;
--- FriendFace.h
+++ FriendFace.h
@@ -39,6 +39,8 @@ class FriendFace{
 		void menu();
 
 		void quit();
+
+		void trim(std::string& s);
 		
 		void listUsers();
 
